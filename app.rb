07f428960b4b4f79a5df094f142b860aa6f565aa
Salomanuel <salomanuel.aux+code@gmail.com>
2017-03-04 14:27:12 +0100

    add undertitle

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require "sinatra"
 require "sinatra/reloader" if development?
 
 get "/" do
-	@title = "Caesar's Cypher"
+	@title   = "Caesar's Cypher"
+	@comment = 'Ruby on Rails >> Sinatra >> <a href="http://www.theodinproject.com/courses/ruby-on-rails/lessons/sinatra-project?ref=lnav">Project: Sinatra Project</a>'
 	erb :index
 end
